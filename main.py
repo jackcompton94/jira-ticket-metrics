cd616ae7d5393da41d0984ea34322d2432aea1a0
jackcompton94 <jackcompton94@gmail.com>
2023-03-16 23:42:41 -0500

    added datadotworld module to auto sync on ingest

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import jira_api
+import datadotworld as dw
 
 jira_api.get_jira_tickets()
+
+# Auto syncs dataset on data.world for real time visibility
+client = dw.api_client()
+client.sync_files('jcorg/all-dws-tickets')
